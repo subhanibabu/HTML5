97d21f28f3001f40e1c9e018669d22e6fb627409
subhanibabu <subhani_anchula@epam.com>
2019-08-03 17:47:40 +0530

    ES8 challenges.
    ES8 challenges.

--- HomeTask4/ES8/Reflect/reflect.challenge.js
+++ HomeTask4/ES8/Reflect/reflect.challenge.js
@@ -1,7 +1,27 @@
+let course = {};
+Reflect.defineProperty(course, "name", {
+    value: "javascript",
+    writable: false
+});
 
-let course = {
+const student = {
+    name: 'Subbu',
+    fee: 42
+};
 
-}
+console.log(`Before deleting property from object : ${student.fee}`); // Expects 42
+
+Reflect.deleteProperty(student, 'fee');
+
+console.log(`After deleting property from object : ${student.fee}`); //Expects undefined
+
+Reflect.defineProperty(course, "duration", {
+    value: "3 hours",
+    writable: false
+});
+
+console.log(course);
+console.log(Reflect.getOwnPropertyDescriptor(course, "name"));
 
 // use  reflect API to define property,  delete a property, getOwnpropertydescriptor details
 // property to define - name with value as "Javascript", make it read only (writable : false)
