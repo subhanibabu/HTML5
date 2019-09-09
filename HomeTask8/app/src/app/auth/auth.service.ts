a6d80edc85c8274485d67e3d963eb9f80f2719f5
subhanibabu <subhani_anchula@epam.com>
2019-09-09 16:49:50 +0530

    Home Task 8 Review Comments.
    Home Task 8 Review Comments.

--- HomeTask8/app/src/app/auth/auth.service.ts
+++ HomeTask8/app/src/app/auth/auth.service.ts
@@ -5,15 +5,10 @@ import { NewsService } from '../services/news.service';
   providedIn: 'root'
 })
 export class AuthService {
-loggedInUserName : string;
   constructor(private newsService : NewsService) { 
   }
 
   get showArticle(): boolean {
-    this.loggedInUserName = this.newsService.getLoggedInUserName;
-    if(this.loggedInUserName === 'admin')
-      return true;
-    else
-      return false;
+    return this.newsService.getLoggedInUserName === 'admin';
   }
 }
